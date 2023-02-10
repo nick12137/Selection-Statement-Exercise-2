844fb0a19d54843861b5042858997bd0e1650aad
Nikolay Loboda <nick12137@gmail.com>
2023-02-09 19:35:37 -0600

    finished exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace SelectionStatementsExercise2
+﻿using System;
+
+
+namespace SelectionStatementsExercise2
 {
-    public class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+
+            var subject = Console.ReadLine(); //Evaluate to a string
+
+            switch (subject)
+            {
+                case "math":
+                    Console.WriteLine("Math is a tough subject!");
+                    break;
+
+                case "english":
+                    Console.WriteLine("English is no fun!");
+                    break;
+
+                case "history":
+                    Console.WriteLine("History is Cool!");
+                    break;
+
+                case "science":
+                    Console.WriteLine("Science is very interesting!");
+                    break;
+
+                case "Art":
+                    Console.WriteLine("Art is very Fun!");
+                    break;
+
+                default:
+                    Console.WriteLine($"Oh wow! I haven't taken that subject before. {subject} sounds cool!");
+                    break;
+            }
+
         }
     }
 }
